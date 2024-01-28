5b9a9ed1a109c7e7654f44a5f17bd247977ac19f
Akash kumar <62200807+kumarduhoon@users.noreply.github.com>
2024-01-28 18:02:45 +0000

    data type

---
+++ my_code/dataType.js
@@ -0,0 +1,16 @@
+"use strict" //this is used for consider the newly version of javacript
+// when we used the use strict key in javacript file it consder as new verion of javacript code because have so many version
+
+// Primitive Data Type
+// Number 
+// bigInt
+// string
+// undefined  :this type in javascript
+// null : this type is an object and a standalone value in javacript 
+// boolean
+// Symbol :  this is for unique identity
+
+
+// No primitive or Object in javacript 
+
+Object
