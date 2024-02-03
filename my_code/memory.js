5c965fe3045e80e026000acdb768c6da08605963
Akash kumar <62200807+kumarduhoon@users.noreply.github.com>
2024-02-03 17:44:03 +0000

    memory in javascript

---
+++ my_code/memory.js
@@ -0,0 +1,4 @@
+//stack memory (all primitive data type are stored in stack memory in javascript, in this memory we get the copy of any data type value)
+
+
+// heap memory(all non primitive data type are stored in head memory in javascript, in this memory we get the reference of the variable when we try to make a copy of that)
