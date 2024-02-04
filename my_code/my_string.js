3ffa5c997e3d3cd80712248370cf029f86841f9f
Akash kumar <62200807+kumarduhoon@users.noreply.github.com>
2024-02-04 09:04:38 +0000

    some string method

---
+++ my_code/my_string.js
@@ -0,0 +1,28 @@
+let mr_var = "javacript"
+let str  = `this is the go way to write the english beacuse we can write the ${mr_var} code inside it without used concate mwthod`
+
+let str1 = "this is only for test "
+// Javascript is provide a lot of in build method for mainpulate the string let discuss one by one 
+
+// 1. anchor method is Deprecated so we are not going to discuss
+
+// 2 at() is used to find the chararter in string based on index
+ console.log(str1.at(5))
+console.log(str1.at(-5))
+// 3.  big() method is Deprecated so we are not going to discuss
+// 4.  blink() method is Deprecated so we are not going to discuss
+// 5.  bold() method is Deprecated so we are not going to discuss
+
+// 6. charAt return the new string as value and it used to find the character based on the index
+
+ console.log(str1.charAt(5))
+ console.log( `char at ${str1.charAt(-5)}`)
+ // note at() and charAt() both are work simliar but there is a differnece at is accept the negative value but charAt() not
+
+// 7. charCodeAt() Every char have a unique code number for represent it so find the charcode of char based on the index
+
+console.log(str1.charCodeAt(5))
+
+// 8. codePointAt() which is used to return the unicode of the character
+
+9. 
