4fc24488e78f42c43b0db9afb9a47c8683e41143
Akash kumar <62200807+kumarduhoon@users.noreply.github.com>
2024-02-04 09:45:28 +0000

    Math and number

---
+++ my_code/math_and_num.js
@@ -0,0 +1,21 @@
+const num = 400;
+
+const num2 =  new Number(100)
+
+ //console.log(num2.toString().length)
+
+// we can convet the number into string and apply all the string method to the sting
+
+//console.log(num2.toFixed(2)) //we can fixed the digit after decimal using this method
+
+// ++++++++++++++++++++++++++++++Math++++++++++++++++++//
+
+
+//console.log(Math.round(34.55))
+//console.log(Math.floor(34.99))
+//console.log(Math.ceil(34.33))
+
+let min = 1 
+let max = 6
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min) // this is code for die 
