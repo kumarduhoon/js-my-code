286c00689e8f1494218fd68ba3ae15781a076e96
Akash kumar <62200807+kumarduhoon@users.noreply.github.com>
2024-02-06 16:41:56 +0000

    more about function

--- my_basic_part_two/function_part.js
+++ my_basic_part_two/function_part.js
@@ -23,3 +23,34 @@ function sumOfTwoNumber(num1, num2) {
 }
 // in function execuation when we passed the  value  this is call "argunment "
 sumOfTwoNumber(2, 3) // here 2 and 3 is argument 
+
+// +++++++++++++++++++++++++++++++++++++++++++++++//
+
+//  how to use "rest opertor" in the javascript function
+//  this is the live example of the rest opertor when we going to calcluate the price of the cart item,so we used the rest operator because item are not fixed in the cart so collect all the dynamic item in cart we used the rest operator  
+
+function calCartPrice(...price) {
+    return price
+}
+
+// console.log(calCartPrice(200, 300, 4000, 400))
+
+//  how to pass the object in the function as an argument
+
+const user = {
+    name: "Akash",
+    age: 26
+}
+
+function userDetails(anyObject) {
+    return anyObject.name
+}
+// console.log(userDetails(user))
+
+// how to pass the array in the function as an argument
+
+const users = [202, 20, 4]
+function userInfo(anyArray) {
+    return anyArray
+}
+console.log(userInfo(users))
