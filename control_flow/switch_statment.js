32f0e7d78f179b296e72a304754015d6e96b579e
Akash kumar <62200807+kumarduhoon@users.noreply.github.com>
2024-02-08 07:32:00 +0000

    switch statment

---
+++ control_flow/switch_statment.js
@@ -0,0 +1,54 @@
+// this is also used execute the code based on the condition 
+// syntax of the switch
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// for example
+let month = "march"
+
+switch (month) {
+    case "january":
+        console.log("this is 1st month")
+        break;
+    case "february":
+        console.log("this is 2nd month")
+        break;
+    case "march":
+        console.log("this is 3rd month")
+        break;
+    case "april":
+        console.log("this is 4th month")
+        break;
+    case "may":
+        console.log("this is 5th month")
+        break;
+    case "june":
+        console.log("this is 6th month")
+        break;
+    case "july":
+        console.log("this is 7th month")
+        break;
+    case "august":
+        console.log("this is 8th month")
+        break;
+    case "september":
+        console.log("this is 9th month")
+        break;
+    case "octemebr":
+        console.log("this is 10th month")
+        break;
+    case "november":
+        console.log("this is 11th month")
+        break;
+    case "December":
+        console.log("this is 12th month")
+        break;
+    default:
+        break;
+}
