fd815dfdf03242d710ef51a279f5eed2ba2648bd
Akash kumar <62200807+kumarduhoon@users.noreply.github.com>
2024-02-08 06:56:48 +0000

    if and else statment

---
+++ control_flow/if_else_if.js
@@ -0,0 +1,49 @@
+// if statment =>we can run the code based on the condition which is increase the performanace the website, beacuse this condition stop the running unwanted code. only execute required code
+
+// if (condition) {  here condition should be true if condition is not true then code is not execute 
+//      code
+// }
+
+let num = 2;
+
+if (num === 2) {
+    // console.log("num value is 2")
+}
+
+// if else is also used execute the code based on the condition but there ia difference is if condition is not matched, then also run some piece of code 
+
+// this is the syntax
+// if (condition) {
+
+// } else {
+
+// }
+
+let num2 = 5
+
+if (num === 6) {
+    // console.log("number is equal to 6")
+} else {
+    // console.log(`number is equal to ${num2}`)
+}
+
+
+// if,else-if is used to check the multiple condition and run the code based of that condition
+
+// if(condition){
+
+// }else if(condition){
+
+// }else if(condition){
+
+// }else{
+
+// }
+
+if (num === 3) {
+    console.log("number is 3")
+} else if (num === 2) {
+    console.log("number is 2")
+} else {
+    console.log("not found")
+}
